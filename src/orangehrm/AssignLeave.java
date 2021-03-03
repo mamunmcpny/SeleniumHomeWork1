680c183a7a59f05b18cee5ff43a3c96c855e80e4
mamunmcpny <mamunmcpny@gmail.com>
2021-03-03 15:44:32 -0500

    03/03/2021

--- src/orangehrm/AssignLeave.java
+++ src/orangehrm/AssignLeave.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
+
 public class AssignLeave extends HelperClass {
 
     public static void leave(String empName) {
@@ -12,10 +14,53 @@ public class AssignLeave extends HelperClass {
 //
         driver.findElement(By.xpath("(//span[@class='quickLinkText'])[1]")).click();
         driver.findElement(By.xpath("//input[@name='assignleave[txtEmployee][empName]']")).sendKeys(empName);
+
         WebElement selectDd = driver.findElement(By.name("assignleave[txtLeaveType]"));
         Select leaveType = new Select(selectDd);
         leaveType.selectByVisibleText("US - Personal");
+        //click on calender //
+        driver.findElement(By.xpath("//input[@name='assignleave[txtFromDate]']")).click();
+        //select date
+        List<WebElement> dates = driver.findElements(By.xpath("//td[@data-handler='selectDay']/a"));
+        System.out.println(" Lenth of Webelements :" + dates.size());
+
+        for (WebElement eachDate : dates) {
+            String eDate = eachDate.getText();
+
+            if (eDate.equalsIgnoreCase("18")) {
+                eachDate.click();
+                break;
+            }
+        }
+        // Select calender to
+        driver.findElement(By.xpath("//input[@name='assignleave[txtToDate]']")).click();
+        //select to date
+        List<WebElement> toDate = driver.findElements(By.xpath("//td[@data-event='click']/a"));
+
+        for (WebElement endDate : toDate) {
+            String finishDate = endDate.getText();
+
+            if (finishDate.equalsIgnoreCase("31")) {
+                endDate.click();
+                break;
+            }
+        }
+// "assignleave[partialDays]"
+        // "Start and End Day"
+        WebElement partialDays = driver.findElement(By.name("assignleave[partialDays]"));
+        Select pDays = new Select(partialDays);
+        pDays.selectByValue("start_end");
+
+        driver.findElement(By.cssSelector("#assignBtn")).click();
+
+        driver.findElement(By.cssSelector("#confirmOkButton")).click();
+
     }
 
-
 }
+
+
+
+
+
+
